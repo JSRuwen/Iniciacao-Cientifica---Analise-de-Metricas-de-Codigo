428d0ae7bc904aebcadb56fb5a99c1dd19c22ec2
Rhuan S. Wolfgramm <124512081+JSRuwen@users.noreply.github.com>
2026-04-10 07:54:57 -0300

    Update helloworld.java

--- venv/java/simples/helloworld.java
+++ venv/java/simples/helloworld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
+		// o loc() vai contar esse comentario e essa linha abaixo
+		
 		System.out.println("Hello, World!");
+		
 	}
 }
